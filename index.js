2d37e2edcfdb9ca4635d9654a1a9c32a5c03fd04
calm-fork-7535 <komald.4@gmail.com>
2020-01-06 23:45:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // add solution here
 function theBeatlesPlay(player, instruments){
-  
-
+  var allPlayers = [];
+  for (var i=0; i < players.length; i++){
+    allPlayers.push(players[i] + "plays" + instruments[i]);
+  }
+  return allPlayers;
 }
