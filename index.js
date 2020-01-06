a8e4173026a7cc1a0d7394d0ba79f348d64a9cf4
calm-fork-7535 <komald.4@gmail.com>
2020-01-06 23:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(players, instruments){
   var allPlayers = [];
   for (var i=0; i < players.length; i++){
-    allPlayers.push(players[i] + "plays" + instruments[i]);
+    allPlayers.push(players[i] + " plays " + instruments[i]);
   }
   return allPlayers;
 }
