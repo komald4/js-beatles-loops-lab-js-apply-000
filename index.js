e058a9e4396564fbbdf78ab28dac468419ce0a15
calm-fork-7535 <komald.4@gmail.com>
2020-01-06 23:57:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(){
 function iLoveTheBeatles(num){
   var love = [];
     do {
-      love.push("I love the Beatle!");
+      love.push("I love the Beatles!");
       num++;
     } while (num < 15);
   return love;
