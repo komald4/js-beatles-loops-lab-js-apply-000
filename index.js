c672454f3097438099c7a9a473c5c497c977ded5
calm-fork-7535 <komald.4@gmail.com>
2020-01-07 00:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ function theBeatlesPlay(players, instruments){
 }
 
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
 }
 
 
